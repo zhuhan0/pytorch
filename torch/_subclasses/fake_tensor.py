35e50ad42f52e561362832e0e7cd2cb0e26a51ab
Joel Schlosser <jbschlosser@meta.com>
2023-05-02 13:05:47 -0400

    (WIP) Use python tensor subclass version of NT for PT2
    ghstack-source-id: 20ccf521913f40eae1f40d7ea0478d74cf691374
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/99753

--- torch/_subclasses/fake_tensor.py
+++ torch/_subclasses/fake_tensor.py
@@ -811,7 +811,19 @@ class FakeTensorConfig:
     debug = os.environ.get("TORCH_FAKE_TENSOR_DEBUG", False)
 
 
-class FakeTensor(torch.Tensor):
+# Hack to make isinstance(x, FakeTensor) return True when x is a fake-ified NestedTensor.
+# A NestedTensor that has been fake-ified simply has a fake buffer.
+class FakeTensorMeta(torch._C._TensorMeta):
+    def __instancecheck__(self, instance):
+        from torch.nested._nested_tensor import NestedTensor
+
+        if isinstance(instance, NestedTensor):
+            return isinstance(instance.buffer, self)
+
+        return super().__instancecheck__(instance)
+
+
+class FakeTensor(torch.Tensor, metaclass=FakeTensorMeta):
     """
     Meta tensors give you the ability to run PyTorch code without having to
     actually do computation through tensors allocated on a `meta` device.
@@ -1104,8 +1116,12 @@ class FakeTensorMode(TorchDispatchMode):
             any([i._has_symbolic_sizes_strides for i in flat_arg_fake_tensors])
             or len(flat_symints) > 0
         )
+        has_nested = any([i.is_nested for i in flat_arg_fake_tensors])
 
         converter = self.fake_tensor_converter
+
+        if has_nested:
+            return func(*args, **kwargs)
 
         # To constant propagate through these functions:
         # 1, If this is a lift, the input tensor is guaranteed to be a
@@ -1207,8 +1223,8 @@ class FakeTensorMode(TorchDispatchMode):
         # is written to must be invalidated
         self.invalidate_written_to_constants(func, flat_arg_fake_tensors, args, kwargs)
 
-        # Try for fastpath
-        if has_symbolic_sizes:
+        # Try for fastpath (not support for nested tensors right now)
+        if has_symbolic_sizes and not has_nested:
             fast_impl = get_fast_op_impls().get(func)
             if fast_impl is not None:
                 return fast_impl(self, *args, **kwargs)
