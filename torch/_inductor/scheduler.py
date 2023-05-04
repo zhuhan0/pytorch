703c1f31380e64641d7714571e0957d4739c464a
Rodrigo Kumpera <kumpera@fb.com>
2023-05-04 15:11:33 +0000

    Rework Inductor support for collectives.
    This is done by introducing two new base classes: InPlaceCollectiveKernel and OutOfPlaceCollectiveKernel.
    
    They deal with the differences for when InPlaceHint needs to be used.
    
    Additionally to that, we introduce `has_side_effects` method to buffers that
    prevents them from being DCE'd by the scheduduler. This is needed because InPlaceHint
    nodes both wrap the inputs and are the outputs, which places no users to the collectives
    themselves.
    
    ghstack-source-id: 525506e6f9a420140a7a0998e5bd34a8b3661afb
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/99765

--- torch/_inductor/scheduler.py
+++ torch/_inductor/scheduler.py
@@ -209,6 +209,9 @@ class BaseSchedulerNode:
     def can_inplace(self, read_dep: dependencies.MemoryDep):
         return False
 
+    def has_side_effects(self):
+        return False
+
     def allocate(self):
         if not self.node.should_allocate():
             return
@@ -335,6 +338,9 @@ class ExternKernelSchedulerNode(BaseSchedulerNode):
 
     def is_extern(self):
         return True
+
+    def has_side_effects(self):
+        return hasattr(self.node, "has_side_effects") and self.node.has_side_effects()
 
     def can_inplace(self, read_dep: dependencies.MemoryDep):
         if self.get_aliases() or self.is_template():
@@ -814,7 +820,7 @@ class Scheduler:
         """
         updated_nodes = []
         for node in self.nodes:
-            if node.users:
+            if node.users or node.has_side_effects():
                 updated_nodes.append(node)
             else:
                 # dead code
