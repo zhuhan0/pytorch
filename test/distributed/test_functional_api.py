ff4d3264bb032e98e584ef684dbb5c1ee1ae858c
Rodrigo Kumpera <kumpera@fb.com>
2023-05-04 15:11:33 +0000

    Implement all_gather_into_tensor_coalesced.
    The implementation is suboptimal since it uses c10d's group coalescing which
    is known to be inneficient.
    
    ghstack-source-id: a06b641af3d5979ef573d51ed9293826824b2930
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/98642

--- test/distributed/test_functional_api.py
+++ test/distributed/test_functional_api.py
@@ -16,10 +16,11 @@ if not dist.is_available():
 
 from torch.testing._internal.common_distributed import (
     MultiThreadedTestCase,
+    skip_if_lt_x_gpu
 )
 from torch.testing._internal.common_utils import (
     run_tests,
-    TestCase
+    TestCase,
 )
 
 def new_subgroups(group_size: int, pg_tag=None):
@@ -233,6 +234,26 @@ class TestTraceableCollectives(MultiThreadedTestCase):
         res = ft_c.all_reduce_coalesced([t0, t1], "sum", mesh)
         self.assertEqual(res[0], t0 * 4)
         self.assertEqual(res[1], t1 * 4)
+
+    def test_all_gather_into_tensor_coalesced(self):
+        tensors = [torch.ones([4], device="cpu"), torch.ones([4], device="cpu") + 1]
+        mesh = dt.DeviceMesh("cpu", torch.arange(4))
+
+        res = ft_c.all_gather_into_tensor_coalesced(tensors, mesh)
+        self.assertEqual(2, len(res))
+        self.assertEqual(torch.ones([4 * dist.get_world_size()]), res[0])
+        self.assertEqual(torch.ones([4 * dist.get_world_size()]) + 1, res[1])
+
+
+    @skip_if_lt_x_gpu(1)
+    def test_all_gather_into_tensor_coalesced_cuda(self):
+        tensors = [torch.ones([4], device="cuda:0"), torch.ones([4], device="cuda:0") + 1]
+        mesh = dt.DeviceMesh("cuda", torch.arange(4))
+
+        res = ft_c.all_gather_into_tensor_coalesced(tensors, mesh)
+        self.assertEqual(2, len(res))
+        self.assertEqual(torch.ones([4 * dist.get_world_size()]), res[0])
+        self.assertEqual(torch.ones([4 * dist.get_world_size()]) + 1, res[1])
 
 
 class TestMetaCollectives(TestCase):
